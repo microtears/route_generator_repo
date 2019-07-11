79ee85cf38558f0dd7d25280bef0f53dcf493210
microtears <microtears@gmail.com>
2019-07-11 22:04:19 +0800

    为RoutePage添加了文档

--- route_annotation/lib/src/route_page.dart
+++ route_annotation/lib/src/route_page.dart
@@ -14,6 +14,15 @@ class RoutePage {
   // final RouteGetter routeGetter;
   final List<RoutePrarm> prarms;
 
+  /// 此注解用来注解一个路由页面
+  ///
+  /// * [name] 路由名称，建议采用下划线方式名称，例如`custom_route_name`，或是采用`/`分割的url路径，例如：`/library/music`。
+  /// * [generatedRoute] 是否自动生成路由，如果值为`false`,需要手动编写路由页面代码，同时指定自定义路由字段名称 [routeFieldName]。
+  /// * [isInitialRoute] 是否是初始化路由页面，如果值为`true`,那么将忽略自定义路由名称 [name] ，同时生成名为`ROUTE_HOME`的路由常量，需要注意的是：只允许有一个初始化路由页面。
+  /// * [isAlias] 是否是别名
+  /// * [routeFieldName] 自定义路由字段名称，默认值为`route`，自定义路由时需要确认此值。
+  /// * [prarms] 路由参数列表，详细请参照 [RoutePrarm]。
+
   // "If generatedRoute==false, you must provide routeGetter to generate route."
   // : assert(generatedRoute == false && routeGetter != null)
   const RoutePage({
